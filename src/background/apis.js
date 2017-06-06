35f07fe6ff89e3c91fa0e16292876b117702f01d
Aiello Chan <Aiello.Chan@gmail.com>
2017-06-06 15:27:43 +0800

    add github apis file

---
+++ src/background/apis.js
@@ -0,0 +1,40 @@
+/*=========================================================
+Github api script
+Use github apis
+==========================================================*/
+
+
+// if specific file exist
+export function Exist(target, cb) {
+
+}
+
+
+// if one file modified
+export function Modified(target, sha, cb) {
+
+}
+
+
+// create a repo
+export function CreateRepo(target, options, token, cb) {
+
+}
+
+
+// create a new file
+export function CreateFile(target, data, message, cb) {
+
+}
+
+
+// get file content
+export function GetContent(target, cb) {
+
+}
+
+
+// update a file
+export function UpdateFile(target, data, cb) {
+
+}
