423fe736de7cdda9d7d12d324385df6e14bf8d35
Aiello Chan <aiello@iMac.local>
2018-01-02 15:37:46 +0800

    Add webpack page debug support

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,7 @@ var path = require('path'),
   CopyWebpackPlugin = require('copy-webpack-plugin'),
   ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const extractLess = new ExtractTextPlugin({
-  filename: "[name].[contenthash].css",
-  disable: process.env.NODE_ENV === "development"
-});
-
-module.exports = {
+var webpackConfig = {
   entry: {
     './app/app': './src/app/app.js',
     './app/background': './src/background/background.js',
@@ -21,34 +16,23 @@ module.exports = {
     filename: '[name].js'
   },
   module: {
-    rules: [{
-      test: /\.vue$/,
-      loader: 'vue-loader',
-      options: {
-        loaders: {}
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {}
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules|vendor/
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=8192'
       }
-    },
-    {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: /node_modules|vendor/
-    },
-    {
-      test: /\.(png|jpg)$/,
-      loader: 'url-loader?limit=8192'
-    },
-    {
-      test: /\.less$/,
-      use: extractLess.extract({
-        use: [{
-          loader: "css-loader"
-        }, {
-          loader: "less-loader"
-        }],
-        // use style-loader in development 
-        fallback: "style-loader"
-      })
-    }
     ]
   },
   plugins: [
@@ -58,36 +42,24 @@ module.exports = {
       filename: "options/index.html",
       inject: false
     }),
-    new CopyWebpackPlugin([{
-      from: 'src/manifest.json'
-    }, {
-      from: 'src/_locales',
-      to: "_locales"
-    }, {
-      from: 'src/assets',
-      to: "assets"
-    }, {
-      from: 'src/vendor',
-      to: "vendor"
-    }]),
-    extractLess
+    new CopyWebpackPlugin([
+      {
+        from: 'src/manifest.json'
+      }, {
+        from: 'src/_locales',
+        to: "_locales"
+      }, {
+        from: 'src/assets',
+        to: "assets"
+      }, {
+        from: 'src/vendor',
+        to: "vendor"
+      }]
+    )
   ],
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
-    }
-  },
-  devServer: {
-    contentBase: [path.join(__dirname, 'dist/options'), path.join(__dirname, 'dist/vendor')],
-    compress: true,
-    noInfo: true,
-    proxy: {
-      "/vendor": {
-        target: "http://localhost:8080",
-        pathRewrite: {
-          "^/vendor": ""
-        }
-      }
     }
   },
   performance: {
@@ -96,10 +68,30 @@ module.exports = {
   devtool: '#eval-source-map'
 }
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+
+if (process.env.NODE_ENV === "development") {
+  webpackConfig.devServer = {
+    contentBase: [
+      path.join(__dirname, 'webpackServer'),
+      path.join(__dirname, 'dist/')
+    ],
+    compress: true,
+    noInfo: true,
+    // proxy: {
+    //   "/vendor": {
+    //     target: "http://localhost:8080",
+    //     pathRewrite: {
+    //       "^/vendor": ""
+    //     }
+    //   }
+    // }
+  }
+}
+
+if (process.env.NODE_ENV === "production") {
+  webpackConfig.devtool = '#cheap-module-eval-source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
+  webpackConfig.plugins = (webpackConfig.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
@@ -116,3 +108,6 @@ if (process.env.NODE_ENV === 'production') {
     })
   ])
 }
+
+
+module.exports = webpackConfig;
